76701e9ac97f718de83e620f142e8e466850878e
Arthur <thurdelima@gmail.com>
2024-01-27 01:55:07 -0300

    feat: add bcrypt to hash passwords

--- src/modules/users/useCases/create-user.usecase.ts
+++ src/modules/users/useCases/create-user.usecase.ts
@@ -1,4 +1,5 @@
 import { HttpException, HttpStatus, Injectable } from "@nestjs/common";
+import {hash} from "bcrypt"
 import { PrismaService } from "src/infra/database/prisma.service";
 import { CreateUserDTO } from "../dto/user.dto";
 
@@ -25,8 +26,13 @@ export class CreateUserUseCase {
             throw new HttpException('User already exists!', HttpStatus.BAD_REQUEST);
         }
 
+        const passwordHashed = await hash(data.password, 10);
+
         return await this.prisma.user.create({
-            data,   
+            data: {
+                ...data,
+                password: passwordHashed
+            }   
         })
     }
 }
