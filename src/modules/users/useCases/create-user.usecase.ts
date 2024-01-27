bd505d5412d63cbb2da56b7b4a92a457d2a98694
Arthur <thurdelima@gmail.com>
2024-01-27 00:46:33 -0300

    fix: adjust treat errors

--- src/modules/users/useCases/create-user.usecase.ts
+++ src/modules/users/useCases/create-user.usecase.ts
@@ -1,4 +1,4 @@
-import { Injectable } from "@nestjs/common";
+import { HttpException, HttpStatus, Injectable } from "@nestjs/common";
 import { PrismaService } from "src/infra/database/prisma.service";
 import { CreateUserDTO } from "../dto/user.dto";
 
@@ -22,7 +22,7 @@ export class CreateUserUseCase {
 
 
         if(user) {
-            throw new Error('User already exists !');
+            throw new HttpException('User already exists!', HttpStatus.BAD_REQUEST);
         }
 
         return await this.prisma.user.create({
