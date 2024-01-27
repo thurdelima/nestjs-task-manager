5501595e2de758799d3deb1245a73a8e8553f48a
Arthur <thurdelima@gmail.com>
2024-01-27 01:52:12 -0300

    feat: user pipes to valid fields

---
+++ src/modules/users/pipe/create-user.validation.pipe.ts
@@ -0,0 +1,23 @@
+import { ArgumentMetadata, HttpException, HttpStatus, Injectable, PipeTransform } from "@nestjs/common";
+import { CreateUserDTO } from "../dto/user.dto";
+
+
+@Injectable()
+export class CreateUserValidationPipe implements PipeTransform {
+
+    transform({name, email, username, password}: CreateUserDTO, metadata: ArgumentMetadata) {
+        
+
+        if(!name || !email || !username || !password) {
+            throw new HttpException('[name, email, user and password id required', HttpStatus.UNPROCESSABLE_ENTITY);
+        }
+
+
+        return {
+            name,
+            email,
+            username,
+            password
+        }
+    }
+}
