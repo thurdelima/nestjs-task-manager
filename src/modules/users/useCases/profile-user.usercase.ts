cf103f1514aced8bba7514a47503d3b59bf012a0
Arthur <thurdelima@gmail.com>
2024-01-31 01:28:49 -0300

    feat: create service get profile user and use it in controller

---
+++ src/modules/users/useCases/profile-user.usercase.ts
@@ -0,0 +1,19 @@
+import { Injectable } from "@nestjs/common";
+import { PrismaService } from "src/infra/database/prisma.service";
+
+
+
+
+
+
+@Injectable()
+export class ProfileUserUseCase {
+
+    constructor(private prisma: PrismaService){}
+
+    async execute(id: string) {
+        return await this.prisma.user.findUnique({
+            where: {id}
+        })
+    }
+}
--- src/modules/users/user.controller.ts
+++ src/modules/users/user.controller.ts
@@ -1,7 +1,9 @@
-import { Body, Controller, Post, UsePipes } from "@nestjs/common";
+import { Body, Controller, Get, Post, Request, UseGuards, UsePipes } from "@nestjs/common";
 import { CreateUserUseCase } from "./useCases/create-user.usecase";
 import { CreateUserDTO } from "./dto/user.dto";
 import { CreateUserValidationPipe } from "./pipe/create-user.validation.pipe";
+import { AuthGuard } from "src/infra/providers/auth-guard.provider";
+import { ProfileUserUseCase } from "./useCases/profile-user.usercase";
 
 
 
@@ -10,13 +12,21 @@ import { CreateUserValidationPipe } from "./pipe/create-user.validation.pipe";
 @Controller('/users')
 export class UserController {
 
-    constructor (private readonly createUserUseCase: CreateUserUseCase) {}
+    constructor(private readonly createUserUseCase: CreateUserUseCase, private readonly profileUserUseCase: ProfileUserUseCase) { }
 
     @Post()
     @UsePipes(new CreateUserValidationPipe())
     async create(@Body() data: CreateUserDTO) {
 
         return await this.createUserUseCase.execute(data);
+    }
+
+    @Get("/profile")
+    @UseGuards(AuthGuard)
+    async profile(@Request() req) {
+
+      return await this.profileUserUseCase.execute(req.user.sub);
+
     }
 
 }
