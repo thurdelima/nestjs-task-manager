dc91d6264d90b5532fddc1f523c597bfd0d13d84
Arthur <thurdelima@gmail.com>
2024-02-15 01:19:16 -0300

    feat: add usecase UploadAvatar in userController and add dependencies in user.module

--- src/modules/users/user.controller.ts
+++ src/modules/users/user.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Post, Request, UploadedFile, UseGuards, UseInterceptors, UsePipes } from "@nestjs/common";
+import { Body, Controller, Get, Post, Put, Request, UploadedFile, UseGuards, UseInterceptors, UsePipes } from "@nestjs/common";
 import { CreateUserUseCase } from "./useCases/create-user.usecase";
 import { CreateUserDTO, FileDTO } from "./dto/user.dto";
 import { CreateUserValidationPipe } from "./pipe/create-user.validation.pipe";
@@ -6,6 +6,7 @@ import { AuthGuard } from "src/infra/providers/auth-guard.provider";
 import { ProfileUserUseCase } from "./useCases/profile-user.usercase";
 import { CreateUserResponseSchemaDTO, CreateUserSchemaDTO } from "./schemas/create-user.schema";
 import { FileInterceptor } from "@nestjs/platform-express";
+import { UploadAvatarUserUseCase } from "./useCases/upload-avatar-user.usecase";
 
 
 
@@ -14,31 +15,37 @@ import { FileInterceptor } from "@nestjs/platform-express";
 @Controller('/users')
 export class UserController {
 
-    constructor(private readonly createUserUseCase: CreateUserUseCase, private readonly profileUserUseCase: ProfileUserUseCase) { }
+  constructor(private readonly createUserUseCase: CreateUserUseCase, private readonly profileUserUseCase: ProfileUserUseCase, private readonly uploadAvatarUserUserCase: UploadAvatarUserUseCase) { }
 
-    @Post()
-    //@UsePipes(new CreateUserValidationPipe())
-    async create(@Body() data: CreateUserSchemaDTO) {
+  @Post()
+  //@UsePipes(new CreateUserValidationPipe())
+  async create(@Body() data: CreateUserSchemaDTO) {
 
-        const user = await this.createUserUseCase.execute(data);
+    const user = await this.createUserUseCase.execute(data);
 
-        return CreateUserResponseSchemaDTO.parse(user);
-    }
+    return CreateUserResponseSchemaDTO.parse(user);
+  }
 
-    @Get("/profile")
-    @UseGuards(AuthGuard)
-    async profile(@Request() req) {
+  @Get("/profile")
+  @UseGuards(AuthGuard)
+  async profile(@Request() req) {
 
-      return await this.profileUserUseCase.execute(req.user.sub);
+    return await this.profileUserUseCase.execute(req.user.sub);
 
-    }
+  }
 
 
-    @Post("/avatar")
-    @UseGuards(AuthGuard)
-    @UseInterceptors(FileInterceptor("file"))
-    async uploadAvatar(@Request() req, @UploadedFile() file: FileDTO) {
-      console.log(file);
-    }
+  @Put("/avatar")
+  @UseGuards(AuthGuard)
+  @UseInterceptors(FileInterceptor("file"))
+  async uploadAvatar(@Request() req, @UploadedFile() file: FileDTO) {
+
+    const result = await this.uploadAvatarUserUserCase.execute({
+      file,
+      idUser: req.user.sub
+    });
+
+    return result;
+  }
 
 }
--- src/modules/users/user.module.ts
+++ src/modules/users/user.module.ts
@@ -3,12 +3,19 @@ import { UserController } from "./user.controller";
 import { CreateUserUseCase } from "./useCases/create-user.usecase";
 import { ProfileUserUseCase } from "./useCases/profile-user.usercase";
 import { PrismaService } from "src/infra/database/prisma.service";
+import { UploadAvatarUserUseCase } from "./useCases/upload-avatar-user.usecase";
+import { IStorage } from "src/infra/providers/storage/storage";
+import { SupabaseStorage } from "src/infra/providers/storage/storage.supabase";
 
 
 @Module({
     imports: [],
     controllers: [UserController],
-    providers: [CreateUserUseCase, ProfileUserUseCase, PrismaService]
+    providers: [CreateUserUseCase, ProfileUserUseCase, UploadAvatarUserUseCase, PrismaService,
+        {
+            provide: IStorage,
+            useClass: SupabaseStorage,
+        }]
 })
 export class UserModule {
 
