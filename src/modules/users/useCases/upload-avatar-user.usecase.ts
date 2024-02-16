5ed87dc62dd6199ca2fee0d390deba6e48a185fe
Arthur <thurdelima@gmail.com>
2024-02-16 00:59:45 -0300

    feat: create method uploadAvatar in database users

--- src/modules/users/useCases/upload-avatar-user.usecase.ts
+++ src/modules/users/useCases/upload-avatar-user.usecase.ts
@@ -2,19 +2,40 @@ import { Injectable } from "@nestjs/common";
 import { AvatarDTO } from "../dto/user.dto";
 import { IStorage } from "src/infra/providers/storage/storage";
 import { PrismaService } from "src/infra/database/prisma.service";
+import { extname } from "path";
 
 
 
 @Injectable()
 export class UploadAvatarUserUseCase {
 
-    constructor(private storage: IStorage, prisma: PrismaService) {}
+    constructor(private storage: IStorage, private prisma: PrismaService) { }
 
     async execute(data: AvatarDTO) {
 
+        const extFile = extname(data.file.originalname);
+        const transformName = `${data.idUser}${extFile}`;
+        data.file.originalname = transformName;
         const file = await this.storage.upload(data.file, 'avatar');
-        console.log(file)
+
+
+        const pathAvatarUser = `avatar/${data.file.originalname}`;
+
+        await this.uploadAvatar(data.idUser, pathAvatarUser)
         return file;
 
     }
+
+    async uploadAvatar(id: string, path: string) {
+        await this.prisma.user.update({
+            data: {
+                avatarUrl: path,
+            },
+            where: {
+                id,
+            },
+        });
+    }
+
+
 }
