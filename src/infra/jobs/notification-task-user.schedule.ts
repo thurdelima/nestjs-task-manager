21fcf9b369d5050565faecd933035fdc2d97fa94
Arthur <thurdelima@gmail.com>
2024-02-23 01:07:40 -0300

    feat: import method find tasks to schedule

--- src/infra/jobs/notification-task-user.schedule.ts
+++ src/infra/jobs/notification-task-user.schedule.ts
@@ -1,5 +1,6 @@
 import { Injectable } from "@nestjs/common";
 import { Cron, CronExpression } from "@nestjs/schedule";
+import { ITaskUserRepository } from "src/modules/tasks/repositories/task-user.repository";
 
 
 
@@ -7,9 +8,14 @@ import { Cron, CronExpression } from "@nestjs/schedule";
 @Injectable()
 export class NotificationTaskUserSchedule {
 
+    constructor(
+        private taskRepository: ITaskUserRepository
+    ){}
+
     @Cron(CronExpression.EVERY_5_SECONDS)
-    getAllTasksDay() {
-        console.log('Tasks OK!');
+    async getAllTasksDay() {
+        const allTasks = await this.taskRepository.findAllStartDay();
+        console.log('Tasks OK!', allTasks);
     }
 
 }
--- src/infra/jobs/schedule.module.ts
+++ src/infra/jobs/schedule.module.ts
@@ -1,6 +1,9 @@
 import { Module } from "@nestjs/common";
 import { NotificationTaskUserSchedule } from "./notification-task-user.schedule";
 import { ScheduleModule } from "@nestjs/schedule";
+import { ITaskUserRepository } from "src/modules/tasks/repositories/task-user.repository";
+import { TaskUserPrismaRepository } from "src/modules/tasks/repositories/prisma/task-user.prisma.repository";
+import { PrismaService } from "../database/prisma.service";
 
 
 
@@ -8,7 +11,12 @@ import { ScheduleModule } from "@nestjs/schedule";
 
 @Module({
     imports: [ScheduleModule.forRoot()],
-    providers: [NotificationTaskUserSchedule]
+    providers: [NotificationTaskUserSchedule, 
+        {
+            provide: ITaskUserRepository,
+            useClass: TaskUserPrismaRepository
+        }
+    ]
 })
 
 export class ScheduleTaskModule {}
