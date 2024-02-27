5574ec3945be8fb1cf79050fdba6e381091f6ffb
Arthur <thurdelima@gmail.com>
2024-02-27 01:48:38 -0300

    feat: import connection microsservice and adjust emit message to microsservice

--- src/infra/jobs/notification-task-user.schedule.ts
+++ src/infra/jobs/notification-task-user.schedule.ts
@@ -1,21 +1,45 @@
-import { Injectable } from "@nestjs/common";
+import { Inject, Injectable } from "@nestjs/common";
+import { ClientProxy } from "@nestjs/microservices";
 import { Cron, CronExpression } from "@nestjs/schedule";
 import { ITaskUserRepository } from "src/modules/tasks/repositories/task-user.repository";
 
 
+type MessageDTO = {
+    email: string;
+    startAt: Date;
+    endAt: Date;
+    name: string;
+    title: string;
+    description: string;
+  };
 
 
 @Injectable()
 export class NotificationTaskUserSchedule {
 
     constructor(
-        private taskRepository: ITaskUserRepository
+        private taskRepository: ITaskUserRepository,
+        @Inject('NOTIFICATION') private readonly notificationClient: ClientProxy
     ){}
 
     @Cron(CronExpression.EVERY_5_SECONDS)
     async getAllTasksDay() {
         const allTasks = await this.taskRepository.findAllStartDay();
-        console.log('Tasks OK!', allTasks);
+
+        if(allTasks) {
+            allTasks.forEach((task) => {
+                const message: MessageDTO = {
+                    name: task.user.name,
+                    description: task.task.description,
+                    title: task.task.title,
+                    email: task.user.email,
+                    endAt: task.task.endAt,
+                    startAt: task.task.startAt,
+                  };
+                  console.log(`=== ENVIANDO NOTIFICATION ==== `);
+                this.notificationClient.emit('task_notification', message);
+            })
+        }
     }
 
 }
--- src/infra/jobs/schedule.module.ts
+++ src/infra/jobs/schedule.module.ts
@@ -4,13 +4,20 @@ import { ScheduleModule } from "@nestjs/schedule";
 import { ITaskUserRepository } from "src/modules/tasks/repositories/task-user.repository";
 import { TaskUserPrismaRepository } from "src/modules/tasks/repositories/prisma/task-user.prisma.repository";
 import { PrismaService } from "../database/prisma.service";
+import { ClientsModule, Transport } from "@nestjs/microservices";
 
 
 
 
 
 @Module({
-    imports: [ScheduleModule.forRoot()],
+    imports: [ScheduleModule.forRoot(),  ClientsModule.register([
+        {
+            name: 'NOTIFICATION',
+            transport: Transport.TCP,
+            options: {port: 30002, host: '127.0.0.1'},
+        }
+    ])],
     providers: [NotificationTaskUserSchedule, 
         {
             provide: ITaskUserRepository,
