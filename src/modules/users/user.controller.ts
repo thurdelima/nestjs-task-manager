a86a37bbdfceec6ec161116b92e62e6814be3c6f
Arthur <thurdelima@gmail.com>
2024-02-29 00:45:06 -0300

    docs: create user schema swagger

--- src/modules/users/user.controller.ts
+++ src/modules/users/user.controller.ts
@@ -4,20 +4,30 @@ import { CreateUserDTO, FileDTO } from "./dto/user.dto";
 import { CreateUserValidationPipe } from "./pipe/create-user.validation.pipe";
 import { AuthGuard } from "../../infra/providers/auth-guard.provider";
 import { ProfileUserUseCase } from "./useCases/profile-user.usercase";
-import { CreateUserResponseSchemaDTO, CreateUserSchemaDTO } from "./schemas/create-user.schema";
+import { CreateUserResponseSchemaDTO, CreateUserSchema, CreateUserSchemaDTO } from "./schemas/create-user.schema";
 import { FileInterceptor } from "@nestjs/platform-express";
 import { UploadAvatarUserUseCase } from "./useCases/upload-avatar-user.usecase";
+import { zodToOpenAPI } from "nestjs-zod";
+import { ApiBody, ApiResponse, ApiTags } from "@nestjs/swagger";
 
+const schemaUserSwagger = zodToOpenAPI(CreateUserSchema)
 
 
 
 
 @Controller('/users')
+@ApiTags('users')
 export class UserController {
 
   constructor(private readonly createUserUseCase: CreateUserUseCase, private readonly profileUserUseCase: ProfileUserUseCase, private readonly uploadAvatarUserUserCase: UploadAvatarUserUseCase) { }
 
   @Post()
+  @ApiBody({
+    description: "Criação de usuário",
+    schema: schemaUserSwagger
+  })
+  @ApiResponse({status: 201, description: "Usuário cadastrado com sucesso"})
+  @ApiResponse({status: 400, description: "User already exists"})
   //@UsePipes(new CreateUserValidationPipe())
   async create(@Body() data: CreateUserSchemaDTO) {
 
